1ee308fc465f7cf8002495a61acfe7559e071c08
mohdnas0001 <mohdnas0001@gmail.com>
2024-06-09 08:47:54 -0700

    services provided

--- src/components/HireTalent.tsx
+++ src/components/HireTalent.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 const HireTalent = () => {
   return (
-    <div className="relative mx-auto my-10 h-[820px] w-full max-w-7xl items-center justify-between bg-[#ECF5FF] lg:p-10">
-      <div className="flex w-full flex-row gap-6">
+    <div className="relative mx-auto my-10 h-auto w-full max-w-7xl items-center justify-between bg-[#ECF5FF] lg:p-10">
+      <div className="flex w-full flex-col justify-between gap-10 lg:flex-row">
         <div className="mt-10 flex flex-col items-start justify-center gap-4 p-4 lg:w-[531px]">
           <p className="text-start font-sans text-4xl font-semibold leading-tight">
             We streamline the process of hiring exceptional teams for your
@@ -15,7 +15,7 @@ const HireTalent = () => {
             perfectly matched to your needs, ensuring your business achieves its
             goals with efficiency and expertise.
           </p>
-          <div className="relative flex w-full items-center justify-start overflow-hidden rounded-lg md:h-12 lg:h-28">
+          <div className="relative flex h-12 w-full items-center justify-start overflow-hidden rounded-lg md:h-12 lg:h-28">
             <Image
               src="/Blur.svg"
               alt="Background Image"
@@ -23,21 +23,72 @@ const HireTalent = () => {
               height={61}
               className="absolute left-0 h-full object-cover"
             />
-            <button className="relative z-10 transform rounded-lg bg-black px-4 py-1 text-[8px] font-bold text-white shadow-lg transition duration-300 ease-in-out hover:scale-105 md:px-8 md:py-1 md:text-xs lg:px-14 lg:py-2 lg:text-base">
+            <button className="relative z-10 transform rounded-lg bg-black px-12 py-2 text-[8px] font-bold text-white shadow-lg transition duration-300 ease-in-out hover:scale-105 md:px-8 md:py-1 md:text-xs lg:px-14 lg:py-2 lg:text-base">
               Hire Talents
             </button>
           </div>
-              </div>
-              <div>
-                  <Image
-                  src="/HireTalent.svg"
-              alt="Background Image"
-              width={153}
-              height={61}
-                  />
-                      
-                  
-              </div>
+        </div>
+        <div className="relative w-full lg:w-auto">
+          <div className="relative h-auto w-full">
+            <Image
+              src="/HireTalent.svg"
+              alt="Main Background Image"
+              width={600}
+              height={600}
+              className="h-auto w-full"
+            />
+            {/* Additional images */}
+            <Image
+              src="/Ellipse1.png"
+              alt="Image 1"
+              width={50}
+              height={50}
+              className="absolute left-24 top-[130px] hidden h-12 w-12 md:block md:h-14 md:w-14 lg:h-16 lg:w-16"
+            />
+            <Image
+              src="/Ellipse2.png"
+              alt="Image 2"
+              width={50}
+              height={50}
+              className="absolute right-24 top-[130px] hidden h-12 w-12 md:block md:h-14 md:w-14 lg:h-16 lg:w-16"
+            />
+            <Image
+              src="/Ellipse3.png"
+              alt="Image 3"
+              width={50}
+              height={50}
+              className="absolute bottom-28 left-24 hidden h-12 w-12 md:block md:h-14 md:w-14 lg:h-16 lg:w-16"
+            />
+            <Image
+              src="/Ellipse4.png"
+              alt="Image 4"
+              width={50}
+              height={50}
+              className="absolute bottom-28 right-24 hidden h-12 w-12 md:block md:h-14 md:w-14 lg:h-16 lg:w-16"
+            />
+            <Image
+              src="/Ellipse4.png"
+              alt="Image 4"
+              width={50}
+              height={50}
+              className="absolute left-1/2 top-1/3  h-24 w-24 -translate-x-1/2 transform  md:h-14 md:w-14 lg:h-16 lg:w-16"
+            />
+            <Image
+              src="/Ellipse5.png"
+              alt="Image 5"
+              width={50}
+              height={50}
+              className="absolute left-1/4 top-1/2 hidden h-12 w-12 -translate-y-1/2 transform md:block md:h-14 md:w-14 lg:h-16 lg:w-16"
+            />
+            <Image
+              src="/Ellipse6.png"
+              alt="Image 6"
+              width={50}
+              height={50}
+              className="absolute bottom-1/2 right-1/4 hidden h-12 w-12 translate-y-1/2 transform md:block md:h-14 md:w-14 lg:h-16 lg:w-16"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 const Testimonial = () => {
   return (
-    <div className="relative mx-auto h-[820px] w-full max-w-7xl px-20 pt-10">
-      {/* Green rectangle background */}
-      <div className="absolute bottom-0 right-0 h-[150px] w-full bg-[#112106]"></div>
+    <div className="relative mx-auto mt-10 h-auto w-full max-w-7xl px-6 md:mt-0 md:px-20 pt-10">
+      {/* Green rectangle background, hidden on smaller screens */}
+      <div className="hidden lg:block absolute bottom-0 right-0 h-[150px] w-full bg-[#112106]"></div>
 
       <div className="relative flex w-full flex-col items-center justify-center">
         <div className="mb-4 flex w-fit flex-row items-center justify-center gap-1 rounded-full border border-gray-200 bg-white px-2 py-0 md:px-4 md:py-1 lg:gap-2">
@@ -18,19 +18,19 @@ const Testimonial = () => {
           >
             <circle cx="3.5" cy="4" r="3.5" fill="#27D939" />
           </svg>
-          <p className="font-sans text-xs font-normal leading-6 text-gray-900 md:text-sm lg:text-base">
-            214 people have something good to say about apex
+          <p className="font-sans text-[8px] font-normal leading-6 text-gray-900 md:text-sm lg:text-base">
+            214 people have something good to say about Apex
           </p>
         </div>
-        <p className="text-center font-sans text-4xl font-semibold leading-10 text-gray-900">
+        <p className="text-center font-sans text-2xl md:text-4xl font-semibold leading-8 md:leading-10 text-gray-900">
           Customers shared their
           <br />
           love for Apex
         </p>
       </div>
-      <div className="mt-2com relative flex w-full items-center justify-center">
-        <div className="flex w-full flex-col items-center justify-center gap-20 lg:w-auto lg:flex-row">
-          <div className="mt-28 flex flex-col items-start justify-center gap-20 text-start font-sans text-base leading-8 lg:w-[550px]">
+      <div className="mt-2 relative flex w-full items-center justify-center">
+        <div className="flex w-full flex-col items-center justify-center gap-10 lg:gap-20 lg:w-auto lg:flex-row">
+          <div className="mt-10 md:mt-28 flex flex-col items-start justify-center gap-10 lg:gap-20 text-start font-sans text-sm md:text-base leading-6 md:leading-8 lg:w-[550px]">
             <p className="font-regular">
               &quot;Apex exceeded our expectations with their exceptional
               service and innovative solutions. Their commitment to excellence
@@ -38,19 +38,20 @@ const Testimonial = () => {
               operations. We highly recommend Apex to any company seeking
               reliable and top-notch support.&quot;
             </p>
-            <div className="mt-20 flex w-full flex-col items-start justify-end font-sans text-white">
-              <p className="text-xl font-bold leading-6">John Gavahni</p>
-              <p className="text-lg font-medium leading-6">
+            <div className="flex w-full flex-col items-start justify-end font-sans text-gray-900 lg:text-white">
+              <p className="text-lg md:text-xl font-bold leading-6">John Gavahni</p>
+              <p className="text-base md:text-lg font-medium leading-6">
                 Director of Versity Enterprises
               </p>
             </div>
           </div>
-          <div>
+          <div className="relative">
             <Image
               width={325}
               height={280}
               alt="testimonial"
               src="/Testimonial.png"
+              className="relative"
             />
           </div>
         </div>
